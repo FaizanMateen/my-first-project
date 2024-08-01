1c11ff980eee42c5e5be8fa226a0085a0b253871
annasfaruqui <annasfaruqui123@gmail.com>
2024-08-01 14:42:32 +0530

    Updated the index.css file an tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black">
+      <h1 className="text-4xl text-red-600 [text-shadow:_0_0_50px_rgb(255_0_0)]">
+        GOOD LUCK, COMRADE! DO IT WELL.....
+      </h1>
+    </div>
+  );
 }
 
 export default App;
